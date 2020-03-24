997c49174ba932c7d7edd59019dbff45b11fd9ae
Nick Gerzen <nick.gerzen@gmail.com>
2020-03-24 14:22:21 +0100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   auto-install.sh
    	new file:   packagelist

--- auto-install.sh
+++ auto-install.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
-date
+# This is a script which should automate the Arch-Linux installation process for my
+# specific device.
+# Bases on https://wiki.archlinux.org/index.php/Installation_guide
+
+STATUS="asd"
+
+# Information for Partitioning
+SWAP="8"
+
+abort() {
+    echo -e "\n${STATUS}"
+    echo "Installation process aborted."
+    exit -1
+}
+
+# Pre-installation
+
+# Update the system clock
+echo -n "Synchronizing system clock..."
+
+#STATUS=$(timedatectl set-ntp true)
+if [ -z $STATUS ]; then
+   echo "DONE"
+else
+    abort
+fi
+
+
