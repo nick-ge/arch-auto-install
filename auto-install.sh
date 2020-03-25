4ef0891315665feeeddcc6d5bec2cace12a42b38
Nick Gerzen <nick.gerzen@gmail.com>
2020-03-25 15:34:20 +0100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -5,80 +5,79 @@
 # Bases on https://wiki.archlinux.org/index.php/Installation_guide
 # The following comments prefixed with "Section:" refer to the (sub)sections in the Arch Wiki's Installation Guide.
 
-# Common variable for storing return values of certain commands
+# Common variable for storing return texts of certain commands
 STATUS=""
+# Common variable for storing return codes of certain commands
 RETURN=0
 
 # Information for Partitioning
 SWAP="8G"
 
-abort() {
-    echo -e "\n${STATUS}"
-    echo "Installation process aborted."
-    exit -1
+# Util function which gets invoked after every command.
+# It checks the given return code and prints the error message and the
+# respective return code in case of an error.
+check_returncode() {
+    local RETURN=${1} ERRORTEXT=${2}
+    if [ $RETURN -eq 0]; then
+        echo "OK"
+        return 0
+    else
+        echo "ERROR"
+        echo -e "\n${STATUS}"
+        echo "=> Installation process aborted"
+        exit $RETURN
+    fi
 }
 
-# Section: Pre-installation
+# Section: Pre-Installation
 
-
-# Subsection: Update the system clock
-echo -n "Synchronizing system clock..."
+## Subsection: Update the system clock
+echo -ne "Synchronizing system clock...\t\t"
 STATUS=$(timedatectl set-ntp true)
-RETURN=$?
-echo $RETURN
-if [ -z $STATUS ] && [ $RETURN -eq 0 ]; then
-   echo "DONE"
-else
-    abort
-fi
+check_returncode $? $STATUS
 
-
-
-# Subsection: Partition the disk
-echo -n "Partition the disk..."
+## Subsection: Partition the disk
+echo -ne "Partition the disk...\t\t"
 
 # sfdisk is a "scriptable" version of fdisk, it receives the "user input" from stdin.
 # Here we're creating two partitions:
 # 1. a swap partition of size SWAP
-# 2. a root partition which takes the remaining available space on the hard drive as its size and is marked as bootable
+# 2. a root partition which takes the remaining available space on the hard drive as its size and marked as bootable
 sfdisk /dev/sda > /dev/null << EOF
     , ${SWAP}, S
     , , , *
 EOF
-RETURN=$?
+check_returncode $? "Partitioning with sfdisk failed"
 
+echo -ne "Verifing Partition table...\t\t"
 # sfdisk -V: verifies the partition table of /dev/sda
 STATUS=$(sfdisk -V /dev/sda)
-if [ "$STATUS" = "/dev/sda:" ]; then
-    echo "DONE"
-else
-    abort
-fi
+check_returncode $? $STATUS
 
 
+## Subsection: Format the partitions
+echo -ne "Formatting root partition...\t\t"
+STATUS=$(mkfs.ext4 -q /dev/sda2)
+check_returncode $? $STATUS
 
-# Subsection: Format the partitions
-echo -n "Formatting partitions..."
-mkfs.ext4 -q /dev/sda2
-RETURN=$?
-echo $RETURN
-mkswap /dev/sda1 > /dev/null
-RETURN=$?
-echo $RETURN
-swapon /dev/sda1 > /dev/null
-RETURN=$?
-echo $RETURN
-echo "DONE"
+echo -ne "Initializing swap partition...\t\t"
+STATUS=$(mkswap /dev/sda1 > /dev/null)
+check_returncode $? $STATUS
 
+echo -ne "Enabling swap partition...\t\t"
+STATUS=$(swapon /dev/sda1 > /dev/null)
+check_returncode $? $STATUS
 
 
-# Subsection: Mounting the file system
-echo -n "Mounting file system to /mnt..."
+## Subsection: Mounting the file system
+echo -ne "Mounting file system to /mnt...\t\t"
 STATUS=$(mount /dev/sda2 /mnt)
-RETURN=$?
-echo $RETURN
-if [ -z $STATUS ]; then
-    echo "DONE"
-else
-    abort
-fi
+check_returncode $? $STATUS
+
+
+# Section: Installation
+
+## Subsection: Select the mirror
+echo -ne "Downloading current mirrorlist...\t\t"
+STATUS=$(curl --silent "https://www.archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4&use_mirror_status=on" | sed 's/#Server/Server/' > /etc/pacman.d/mirrorlist)
+check_returncode $? $STATUS
