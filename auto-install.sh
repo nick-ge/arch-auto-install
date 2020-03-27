04406a0a903f700255114465eb97ec830bcb7611
Nick Gerzen <nick.gerzen@gmail.com>
2020-03-27 11:57:26 +0100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -93,48 +93,48 @@ echo -ne "Generating an fstab file...\t"
 ERROR=$(genfstab -U /mnt 2>&1 1>>/mnt/etc/fstab)
 check_returncode $? $ERROR
 
-## Subsection: Chroot
-echo -ne "Chrooting into new system...\t"
-ERROR=$(arch-chroot /mnt 2>&1 1>/dev/null)
-check_returncode $? $ERROR
-
-## Subsection: Time zone
-echo -ne "Setting time zone...\t"
-ERROR=$(ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime 2>&1 1>/dev/null)
-check_returncode $? $ERROR
-
-echo -ne "Syncing hardware clock...\t"
-ERROR=$(hwclock --systohc 2>&1 1>/dev/null)
-check_returncode $? $ERROR
-
-## Subsection: Localization
-echo -ne "Uncommenting needed locales...\t"
-ERROR=$(sed 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen 2>&1 1>/etc/locale.gen)
-check_returncode $? $ERROR
-
-echo -ne "Creating /etc/locale.conf...\t"
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-check_returncode $? $ERROR
-
-echo -ne "Making keyboard settings persistent...\t"
-echo "KEYMAP=de-latin1" > /etc/vconsole.conf
-check_returncode $? $ERROR
-
-## Subsection: Network configuration
-read -p "Enter hostname: " hostname
-echo -ne "Creating hostname file...\t"
-ERROR=$(echo "${hostname}" 2>&1 1>/etc/hostname)
-check_returncode $? $ERROR
-
-echo -ne "Add entry to hosts...\t"
-ERROR=$(echo -e "127.0.0.1\t${hostname}.localdomain ${hostname}" >> /etc/hosts)
-check_returncode $? $ERROR
-
-## Subsection: Initramfs
-## Needed later when encrypting hard drives
-
-## Subsection: Root password
-#echo -ne "Root password needed..."
+### Subsection: Chroot
+#echo -ne "Chrooting into new system...\t"
+#ERROR=$(arch-chroot /mnt 2>&1 1>/dev/null)
+#check_returncode $? $ERROR
+#
+### Subsection: Time zone
+#echo -ne "Setting time zone...\t"
+#ERROR=$(ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime 2>&1 1>/dev/null)
+#check_returncode $? $ERROR
+#
+#echo -ne "Syncing hardware clock...\t"
+#ERROR=$(hwclock --systohc 2>&1 1>/dev/null)
+#check_returncode $? $ERROR
+#
+### Subsection: Localization
+#echo -ne "Uncommenting needed locales...\t"
+#ERROR=$(sed 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen 2>&1 1>/etc/locale.gen)
+#check_returncode $? $ERROR
+#
+#echo -ne "Creating /etc/locale.conf...\t"
+#echo "LANG=en_US.UTF-8" > /etc/locale.conf
+#check_returncode $? $ERROR
+#
+#echo -ne "Making keyboard settings persistent...\t"
+#echo "KEYMAP=de-latin1" > /etc/vconsole.conf
+#check_returncode $? $ERROR
+#
+### Subsection: Network configuration
+#read -p "Enter hostname: " hostname
+#echo -ne "Creating hostname file...\t"
+#ERROR=$(echo "${hostname}" 2>&1 1>/etc/hostname)
+#check_returncode $? $ERROR
+#
+#echo -ne "Add entry to hosts...\t"
+#ERROR=$(echo -e "127.0.0.1\t${hostname}.localdomain ${hostname}" >> /etc/hosts)
+#check_returncode $? $ERROR
+#
+### Subsection: Initramfs
+### Needed later when encrypting hard drives
+#
+### Subsection: Root password
+##echo -ne "Root password needed..."
 
 
 
