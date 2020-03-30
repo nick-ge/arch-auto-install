b9c57c968a3571ce51c8ae96205b0f87b02bb782
Nick Gerzen <nick.gerzen@gmail.com>
2020-03-30 16:49:08 +0200

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   auto-install.sh
    	renamed:    chrooted.sh -> pre/chrooted.sh
    	renamed:    packagelist -> pre/packagelist
    	renamed:    partition-table.conf -> pre/partition-table.conf
    	new file:   pre/partitioning.sh

--- auto-install.sh
+++ auto-install.sh
@@ -3,15 +3,12 @@
 # This is a script which should automate the Arch-Linux installation process for my
 # specific device.
 # Bases on https://wiki.archlinux.org/index.php/Installation_guide
-# The following comments prefixed with "Section:" refer to the (sub)sections in the Arch Wiki's Installation Guide.
+
+HORIZONTALE="========================================================================"
+SUBHORIZONTALE="===================================="
 
 # Common variable for storing return texts of certain commands
 ERROR=""
-
-# Some pseudo GUI variables
-HORIZONTALE="=========================================================================="
-SUBHORIZONTALE="====================================="
-
 
 # Util function which gets invoked after every command.
 # It checks the given return code and prints the error message and the
@@ -38,41 +35,7 @@ echo -ne "Synchronizing system clock...\t\t\t"
 ERROR=$(timedatectl set-ntp true 2>&1 1>/dev/null)
 check_returncode $? $ERROR
 
-echo -e "\n$SUBHORIZONTALE"
-echo -e "\tPartitioning"
-echo -e "$SUBHORIZONTALE\n"
-
-echo -ne "Reading partition table...\t\t\t"
-PARTITIONTABLE=$(cat partition-table.conf)
-check_returncode $? "arch-auto-install/partition-table.conf not found"
-
-echo -ne "Apply partition instructions...\t\t\t"
-# sfdisk is a "scriptable" version of fdisk, it receives the "user input" from stdin.
-sfdisk /dev/sda 1>/dev/null 2>&1 << EOF
-    $PARTITIONTABLE
-EOF
-check_returncode $? "Partitioning with sfdisk failed"
-
-
-echo -ne "Verifing Partition table...\t\t\t"
-# sfdisk -V: verifies the partition table of /dev/sda
-ERROR=$(sfdisk -V /dev/sda 2>&1 1>/dev/null)
-check_returncode $? $ERROR
-
-
-## Subsection: Format the partitions
-echo -ne "Formatting root partition...\t\t\t"
-ERROR=$(mkfs.ext4 -q /dev/sda1 2>&1 1>/dev/null)
-check_returncode $? $ERROR
-
-echo -ne "Initializing swap partition...\t\t\t"
-ERROR=$(mkswap /dev/sda2 2>&1 1>/dev/null)
-check_returncode $? $ERROR
-
-echo -ne "Enabling swap partition...\t\t\t"
-ERROR=$(swapon /dev/sda2 2>&1 1>/dev/null)
-check_returncode $? $ERROR
-
+./pre/partitioning.sh
 
 ## Subsection: Mounting the file system
 echo -ne "Mounting file system to /mnt...\t\t\t"
---
+++ pre/partitioning.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+HORIZONTALE="========================================================================"
+SUBHORIZONTALE="===================================="
+
+# Util function which gets invoked after every command.
+# It checks the given return code and prints the error message and the
+# respective return code in case of an error.
+check_returncode() {
+    local RETURN=${1} ERRORTEXT=${2}
+    if [ $RETURN -eq 0 ]; then
+        echo "OK"
+        return 0
+    else
+        echo -e "ERROR\n${ERRORTEXT}"
+        echo "=> Installation process aborted"
+        exit $RETURN
+    fi
+}
+
+echo -e "\n$SUBHORIZONTALE"
+echo -e "\tPartitioning"
+echo -e "$SUBHORIZONTALE\n"
+
+echo -ne "Reading partition table...\t\t\t"
+PARTITIONTABLE=$(cat partition-table.conf)
+check_returncode $? "arch-auto-install/partition-table.conf not found"
+
+echo -ne "Applying partition instructions...\t\t\t"
+# sfdisk is a "scriptable" version of fdisk, it receives the "user input" from stdin.
+sfdisk /dev/sda 1>/dev/null 2>&1 <<- EOF
+    $PARTITIONTABLE
+EOF
+check_returncode $? "Partitioning with sfdisk failed"
+
+echo -ne "Verifying Partition table...\t\t\t"
+# sfdisk -V: verifies the partition table of /dev/sda
+ERROR=$(sfdisk -V /dev/sda 2>&1 1>/dev/null)
+check_returncode $? $ERROR
+
+## Subsection: Format the partitions
+echo -ne "Formatting root partition...\t\t\t"
+ERROR=$(mkfs.ext4 -q /dev/sda1 2>&1 1>/dev/null)
+check_returncode $? $ERROR
+
+echo -ne "Initializing swap partition...\t\t\t"
+ERROR=$(mkswap /dev/sda2 2>&1 1>/dev/null)
+check_returncode $? $ERROR
+
+echo -ne "Enabling swap partition...\t\t\t"
+ERROR=$(swapon /dev/sda2 2>&1 1>/dev/null)
+check_returncode $? $ERROR
+
+exit 0
