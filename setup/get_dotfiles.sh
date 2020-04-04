1915e74e8b37d3e144b176369f2af0748d928b08
Nick Gerzen <nick.gerzen@gmail.com>
2020-04-04 09:58:15 +0200

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   setup/get_dotfiles.sh

--- setup/get_dotfiles.sh
+++ setup/get_dotfiles.sh
@@ -8,6 +8,7 @@ check_returncode() {
         echo "OK"
         return 0
     else
+        echo "ERROR"
         echo "${ERRORTEXT}" >&2
         exit $RETURN
     fi
@@ -24,39 +25,32 @@ check_returncode $? "$ERROR"
 
 echo -e "\nCloning repository 'nick-ge/dot-files'..."
 ERROR=$(git clone -q git@github.com:nick-ge/dot-files.git ~/dot-files 2>&1)
-rt=$?
+check_returncode $? "$ERROR"
 
-if [ $rt -eq 0 ] && [ -d "~/dot-files" ]; then
+echo -ne "\nCopying git directory...\t\t\t"
+ERROR=$(cp -r ~/dot-files/.git ~/.dotfiles.git 2>&1 1>/dev/null)
+check_returncode $? "$ERROR"
 
-    echo -ne "Copying git directory...\t\t\t"
-    ERROR=$(cp -r ~/dot-files/.git ~/.dotfiles.git 2>&1 1>/dev/null)
-    check_returncode $? "$ERROR"
+# Active globbing on hidden-/dotfiles
+shopt -s dotglob
 
-    # Active globbing on hidden-/dotfiles
-    shopt -s dotglob
+echo -ne "Moving dotfiles to home...\t\t\t"
+ERROR=$(mv ~/dot-files/* ~/. 2>&1 1>/dev/null)
+check_returncode $? "$ERROR"
 
-    echo -ne "Moving dotfiles to home...\t\t\t"
-    ERROR=$(mv ~/dot-files/* ~/. 2>&1 1>/dev/null)
-    check_returncode $? "$ERROR"
+echo -ne "Removing repository from home...\t\t"
+ERROR=$(rm -r ~/dot-files 2>&1 1>/dev/null)
+check_returncode $? "$ERROR"
 
-    echo -ne "Removing repository from home...\t\t"
-    ERROR=$(rm -r ~/dot-files 2>&1 1>/dev/null)
-    check_returncode $? "$ERROR"
+echo -ne "Creating 'exclude' file...\t\t\t"
+ERROR=$(echo "*" 2>&1 1>>~/.dotfiles.git/info/exclude)
+check_returncode $? "$ERROR"
 
-    echo -ne "Creating 'exclude' file...\t\t\t"
-    ERROR=$(echo "*" 2>&1 1>>~/.dotfiles.git/info/exclude)
-    check_returncode $? "$ERROR"
+#echo -ne "Changing owner to nick...\t\t\t"
+#ERROR=$(chown -R nick:users ~/* 2>&1 1>/dev/null)
+#check_returncode $? "$ERROR"
 
-    #echo -ne "Changing owner to nick...\t\t\t"
-    #ERROR=$(chown -R nick:users ~/* 2>&1 1>/dev/null)
-    #check_returncode $? "$ERROR"
-
-    # Deactive globbing
-    shopt -u dotglob
-else
-    echo -e "=> Something went wrong while cloning!"
-    echo "$ERROR" >&2
-    exit $rt
-fi
+# Deactive globbing
+shopt -u dotglob
 
 exit 0
